2bff5c40e503f86a8a470204bf746a19b550486d
indra <indrakumarbanshkar.jd@gmail.com>
2024-03-21 08:42:36 +0530

    addded Collection

---
+++ Java/Udemy/src/Collection/ListMethods.java
@@ -0,0 +1,14 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethods {
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList<>();
+        for (int i = 0; i <+100 ; i=i+10) {
+            list.add(i);
+            
+        }
+    }
+}
