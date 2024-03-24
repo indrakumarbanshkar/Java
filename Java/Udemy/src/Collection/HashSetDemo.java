f27e163c2a18601d11c387955b3d203efecff35b
indra <indrakumarbanshkar.jd@gmail.com>
2024-03-24 09:18:36 +0530

    added hashSetDemo

---
+++ Java/Udemy/src/Collection/HashSetDemo.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.*;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        Random obj = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i <=10 ; i++) {
+            int number = obj.nextInt(5);
+            list.add(number);
+
+        }
+        System.out.println(list);
+        Set<Integer> set = new HashSet<>(list);
+        System.out.println(set);
+    }
+}
