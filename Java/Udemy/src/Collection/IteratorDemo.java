e1f916a96ac2966881c258891c3aad3e16dde4cc
indra <indrakumarbanshkar.jd@gmail.com>
2024-03-25 07:49:22 +0530

    addeded Iterator class

---
+++ Java/Udemy/src/Collection/IteratorDemo.java
@@ -0,0 +1,18 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(30);
+        Iterator<Integer> itr =  list.listIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+            itr.remove();  /* remove all element rom list */
+        }
+        System.out.println(list);
+    }
+}
