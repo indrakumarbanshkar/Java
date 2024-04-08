63913b42782a77e40a24434d9229975ade86b8a3
indra <indrakumarbanshkar.jd@gmail.com>
2024-04-08 18:32:18 +0530

    added files

---
+++ Java/Udemy/src/StringMethods/StringMethods.java
@@ -0,0 +1,13 @@
+package StringMethods;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+        String text = "Hello World";
+        System.out.println(text.toUpperCase());   // Outputs "HELLO WORLD"
+        System.out.println(text.toLowerCase());   // Outputs "hello world"
+       double x=  Math.abs(-4.7);
+        System.out.println(x);
+    }
+}
